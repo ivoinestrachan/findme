7b3b4023a7b1645d3b104e792e2991d0e65ba722
Ivoine <ivoinestrachan242@gmail.com>
2024-05-03 19:44:35 -0700

    fixing env stuff

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ const nextConfig = {
 
   env: {
     API_KEY: process.env.API_KEY,
+    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING
   },
   images: {
     remotePatterns: [
